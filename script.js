89da4139532c73e155cb861fee6c7040cc06d369
Clément Duveau <clement@duveau.eu>
2024-04-15 11:45:48 +0200

    Total et PFU

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function compute() {
     const discount = validate('discount');
     const numberOfActions = validate('numberOfActions');
     const taxRate = validate('taxRate') / 100;
-    if (validationErrors > 0){return;}
+    if (validationErrors > 0) { return; }
 
-    const tauxPrelevementsSociaux = (9.2 + 0.5 + 7.5)/100
+    const tauxPrelevementsSociaux = (9.2 + 0.5 + 7.5) / 100
     const PFU = 12.8 / 100;
 
     // Rabais
@@ -17,11 +17,9 @@ function compute() {
     var rabaisExcedentaireSociaux = rabaisExcedentaireTotal * tauxPrelevementsSociaux;
     var rabaisExcedentaireTaxes = rabaisExcedentaireImpot + rabaisExcedentaireSociaux;
 
-    display(rabaisExcedentaireTotal, 'rabaisExcedentaireTotal');
     display(rabaisExcedentaireImpot, 'rabaisExcedentaireImpot');
     display(rabaisExcedentaireSociaux, 'rabaisExcedentaireSociaux');
     display(rabaisExcedentaireTaxes, 'rabaisExcedentaireTaxes');
-    document.getElementById('rabaisExcedentaire').removeAttribute('hidden');
 
     // Levée d'options
     var leveeOptionTotal = Math.max((marketValue - (pricePaid + discount)) * numberOfActions, 0);
@@ -29,43 +27,44 @@ function compute() {
     var leveeOptionSociaux = leveeOptionTotal * (tauxPrelevementsSociaux + 10 / 100)
     var leveeOptionTaxes = leveeOptionImpot + leveeOptionSociaux;
 
-    display(leveeOptionTotal, 'leveeOptionTotal');
     display(leveeOptionImpot, 'leveeOptionImpot');
     display(leveeOptionSociaux, 'leveeOptionSociaux');
     display(leveeOptionTaxes, 'leveeOptionTaxes');
-    document.getElementById('leveeOption').removeAttribute('hidden');
 
     // Plus value
+    var withPFU = (taxRate > PFU);
+
     var plusValueTotal = Math.max((priceSold - marketValue), 0) * numberOfActions;
-    var plusValueImpot = Math.min(plusValueTotal * PFU, plusValueTotal * taxRate);
+    var plusValueImpot = withPFU ? plusValueTotal * PFU : plusValueTotal * taxRate;
     var plusValueSociaux = plusValueTotal * tauxPrelevementsSociaux
     var plusValueTaxes = plusValueImpot + plusValueSociaux;
 
-    display(plusValueTotal, 'plusValueTotal');
     display(plusValueImpot, 'plusValueImpot');
     display(plusValueSociaux, 'plusValueSociaux');
     display(plusValueTaxes, 'plusValueTaxes');
-    document.getElementById('plusValue').removeAttribute('hidden');
 
     // Total
-    var taxImpot = rabaisExcedentaireImpot + leveeOptionImpot;
-    var taxSociaux = rabaisExcedentaireSociaux + leveeOptionSociaux;
+    var taxImpot = rabaisExcedentaireImpot + leveeOptionImpot + plusValueImpot;
+    var taxSociaux = rabaisExcedentaireSociaux + leveeOptionSociaux + plusValueSociaux;
     var taxTaxes = taxImpot + taxSociaux
 
     display(taxImpot, 'taxImpot');
     display(taxSociaux, 'taxSociaux');
     display(taxTaxes, 'taxTaxes');
 
+    document.getElementById('summary').innerText = `Votre achat est de ${Math.round(pricePaid * numberOfActions)}€, vous devrez ${Math.round(taxTaxes - plusValueTaxes)}€ de taxes en plus à l'achat. Lors de la vente, ${withPFU ? 'grâce au PFU,' : ','} vous devrez ${plusValueTaxes}€.`;
 
     document.getElementById('resultHr').removeAttribute('hidden');
     document.getElementById('resultTitle').removeAttribute('hidden');
     document.getElementById('result').removeAttribute('hidden');
+    document.getElementById('summary').removeAttribute('hidden');
 }
 
 function display(value, key) {
     const formatter = new Intl.NumberFormat('fr-FR', {
         style: 'currency',
         currency: 'EUR',
+        minimumFractionDigits: 0,
         maximumFractionDigits: 0
     });
 
@@ -78,7 +77,7 @@ function validate(key) {
 
     element.removeAttribute('aria-invalid');
 
-    if(value < 0 || value === ''){
+    if (value < 0 || value === '') {
         element.setAttribute('aria-invalid', true);
         validationErrors++;
         return;
